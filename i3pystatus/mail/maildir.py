b012de87feaa59ff253b4eeb58e66cd62883932c
Arvedui <asseon@posteo.de>
2018-03-13 19:54:15 +0100

    maildir: also count mails with the seen flag unset in the cur dir

--- i3pystatus/mail/maildir.py
+++ i3pystatus/mail/maildir.py
@@ -19,8 +19,27 @@ class MaildirMail(Backend):
 
     @property
     def unread(self):
-        path = os.path.join(self.directory, "new")
-        return len(os.listdir(path))
+        def check_seen_flag(msgname):
+            """
+            Return false if (S)een flag set
+
+            The code of this funciton was partialy extrated from
+            Pythons Maildir and MaildirMessage classes. Which are not used
+            because they cannot read the message flags without reading the entire message.
+            """
+            maildir_info = msgname.split(':')[-1]
+            # This is a logical implication if maildir_info starts with '2,'
+            # it must not contain S if it does not start with '2,' the rest of
+            # its content does not matter because no flags are set
+            return not maildir_info.startswith('2,') or 'S' not in maildir_info[2:]
+
+        path_new = os.path.join(self.directory, "new")
+        new_messages = len(os.listdir(path_new))
+
+        path_cur = os.path.join(self.directory, "cur")
+        unread_messages = len(list(filter(check_seen_flag, os.listdir(path_cur))))
+
+        return new_messages + unread_messages
 
 
 Backend = MaildirMail
